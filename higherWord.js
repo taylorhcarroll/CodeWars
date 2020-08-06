ace79456042f1d4167893f983d2273d6efb032e1
Taylor Harrison Carroll <54039744+taylorhcarroll@users.noreply.github.com>
2020-08-06 11:44:50 -0500

    Update higherWord.js

--- higherWord.js
+++ higherWord.js
@@ -1,3 +1,6 @@
+//link to ASCII value table
+//https://res.cloudinary.com/practicaldev/image/fetch/s--2xoVYXR3--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://thepracticaldev.s3.amazonaws.com/i/gcsd9q3utce801qbfghq.jpg
+
 function high(x){
   // 1. split x by ' ' to an array.
   const wordList = x.split(' ');
